f1751d6d400f3649b64c4e2dddb3d5a90ccd4c82
di229 <169496356+di229@users.noreply.github.com>
2024-10-30 18:43:11 +0000

    base64 EVP wrapper

--- examples/crypto/base64_example.cc
+++ examples/crypto/base64_example.cc
@@ -4,18 +4,18 @@
 
 int main()
 {
-	if (auto data = DecodeBase64File("base64_sample.txt"))
-	{
+  if (auto data = DecodeBase64File("base64_sample.txt"))
+  {
     printf("decoded %zu bytes\n", data->size());
     if (data->size() > 4) {
       printf("%c %c %c %c\n", data->at(0), data->at(1), data->at(2), 
           data->at(3));
     }
-	  return 0;
-	}
-	else
-	{
-		printf("decode/open failed\n");
+    return 0;
+  }
+  else
+  {
+    printf("decode/open failed\n");
     return 1;
-	}
+  }
 }
--- usefulcxx/crypto/Base64.cc
+++ usefulcxx/crypto/Base64.cc
@@ -5,43 +5,43 @@
 
 OptBytes DecodeBase64String(std::string& str)
 {
-	if (auto ctx = EVP_ENCODE_CTX_new()) 
-	{
-		EVP_DecodeInit(ctx);
-		unsigned char *inp = reinterpret_cast<unsigned char*>(&str[0]);
-		auto inlen = TrimTrailing(str);
-		auto outlen = (3 * inlen) / 4;
-		std::vector<char> res(outlen, 0);
-		unsigned char *outp = reinterpret_cast<unsigned char*>(&res[0]);
+  if (auto ctx = EVP_ENCODE_CTX_new()) 
+  {
+    EVP_DecodeInit(ctx);
+    unsigned char *inp = reinterpret_cast<unsigned char*>(&str[0]);
+    auto inlen = TrimTrailing(str);
+    auto outlen = (3 * inlen) / 4;
+    std::vector<char> res(outlen, 0);
+    unsigned char *outp = reinterpret_cast<unsigned char*>(&res[0]);
 
-		auto rc = EVP_DecodeUpdate(ctx, outp, &outlen, inp, inlen);
+    auto rc = EVP_DecodeUpdate(ctx, outp, &outlen, inp, inlen);
 
-		if (rc != -1) 
-		{
-			int tmp;
-			rc = EVP_DecodeFinal(ctx, outp + outlen, &tmp);
-			outlen += tmp;
-			/* internal whitespace is not decoded*/
-			res.resize(outlen);
-		}
+    if (rc != -1) 
+    {
+      int tmp;
+      rc = EVP_DecodeFinal(ctx, outp + outlen, &tmp);
+      outlen += tmp;
+      /* internal whitespace is not decoded*/
+      res.resize(outlen);
+    }
 
-		EVP_ENCODE_CTX_free(ctx);
+    EVP_ENCODE_CTX_free(ctx);
 
-		if (rc == 1)
-		{
-			return res;
-		}
+    if (rc == 1)
+    {
+      return res;
+    }
 
-	}
-	// decode failed
-	return {};
+  }
+  // decode failed
+  return {};
 }
 
 OptBytes DecodeBase64File(std::string_view path)
 {
-	if (auto str = ReadFile(path))
-	{
-		return DecodeBase64String(*str);
-	}
-	return {};
+  if (auto str = ReadFile(path))
+  {
+    return DecodeBase64String(*str);
+  }
+  return {};
 }
